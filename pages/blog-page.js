1d1cd2571c41a1897f01fe761b30cebae0cbc590
yoh.nishizawa <nishizawa@fork.co.jp>
2022-03-29 18:20:34 +0900

    Implement getAllPostsData on blog-page.js

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,16 @@
 import Layout from "../components/Layout"
+import Post from "../components/Post"
 import Link from "next/link"
-
-export default function BlogPage() {
+import { getAllPostsData } from "../lib/post"
+export default function BlogPage({ filteredPosts }) {
   return (
     <Layout title='Blog page'>
+      <ul>
+        {
+          filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)
+        }
+      </ul>
       <Link href="/main-page">
         <div className='flex cursor-pointer mt-12'>
           <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
@@ -12,4 +19,11 @@ export default function BlogPage() {
       </Link>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData()
+  return {
+    props: { filteredPosts },
+  }
 }

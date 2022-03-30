394999f01047f59ef36d15ec96dd5675848c870e
yoh.nishizawa <nishizawa@fork.co.jp>
2022-03-30 11:47:22 +0900

    implement ISR and CSR on task-page

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,9 +1,31 @@
+import { useEffect } from "react"
 import Layout from "../components/Layout"
+import Task from "../components/Task"
 import Link from "next/link"
+import { getAllTasksData } from "../lib/task"
+import useSWR from "swr"
 
-export default function TaskPage() {
+const fetcher = (url) => fetch(url).then((res) => res.json())
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
+
+export default function TaskPage({ staticFilteredTasks }) {
+
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticFilteredTasks,
+  })
+
+  const filteredTaks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  )
+  useEffect(() => {
+    mutate()
+  }, [])
   return (
     <Layout title='Task page'>
+      <ul>
+        { filteredTaks &&
+          filteredTaks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
       <Link href="/main-page" passHref>
         <div className='flex cursor-pointer mt-12'>
           <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
@@ -12,4 +34,13 @@ export default function TaskPage() {
       </Link>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const staticFilteredTasks = await getAllTasksData()
+
+  return {
+    props: { staticFilteredTasks },
+    revalidate: 3,
+  }
 }

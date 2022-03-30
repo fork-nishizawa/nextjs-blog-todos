f7d796a965de4359bbcee20aefc2f45ddad937b8
yoh.nishizawa <nishizawa@fork.co.jp>
2022-03-30 18:58:31 +0900

    Implement feature of create and update task

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react"
 import Layout from "../components/Layout"
 import Task from "../components/Task"
+import TaskForm from "../components/TaskForm"
 import Link from "next/link"
 import { getAllTasksData } from "../lib/task"
 import useSWR from "swr"
+import StateContextProvider from "../context/StateContext"
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`
@@ -21,18 +23,21 @@ export default function TaskPage({ staticFilteredTasks }) {
     mutate()
   }, [])
   return (
-    <Layout title='Task page'>
-      <ul>
-        { filteredTaks &&
-          filteredTaks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate}/>)}
-      </ul>
-      <Link href="/main-page" passHref>
-        <div className='flex cursor-pointer mt-12'>
-          <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title='Task page'>
+        <TaskForm taskCreated={mutate} />
+        <ul>
+          { filteredTaks &&
+            filteredTaks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate}/>)}
+        </ul>
+        <Link href="/main-page" passHref>
+          <div className='flex cursor-pointer mt-12'>
+            <svg className="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   )
 }
 
